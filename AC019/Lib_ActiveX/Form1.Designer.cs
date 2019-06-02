695b0507039a1a4c64874e19bf32779dda82f9d0
Afterburner <rockey1024@icloud.com>
2019-06-03 00:58:08 +0800

    Just init

---
+++ AC019/Lib_ActiveX/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Lib_ActiveX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axCogDisplay1 = new AxCognex.VisionPro.Interop.AxCogDisplay();
+            this.cogToolBlockEditV21 = new Cognex.VisionPro.ToolBlock.CogToolBlockEditV2();
+            ((System.ComponentModel.ISupportInitialize)(this.axCogDisplay1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cogToolBlockEditV21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axCogDisplay1
+            // 
+            this.axCogDisplay1.Enabled = true;
+            this.axCogDisplay1.Location = new System.Drawing.Point(43, 29);
+            this.axCogDisplay1.Name = "axCogDisplay1";
+            this.axCogDisplay1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axCogDisplay1.OcxState")));
+            this.axCogDisplay1.Size = new System.Drawing.Size(192, 192);
+            this.axCogDisplay1.TabIndex = 0;
+            // 
+            // cogToolBlockEditV21
+            // 
+            this.cogToolBlockEditV21.AllowDrop = true;
+            this.cogToolBlockEditV21.ContextMenuCustomizer = null;
+            this.cogToolBlockEditV21.Location = new System.Drawing.Point(12, 12);
+            this.cogToolBlockEditV21.MinimumSize = new System.Drawing.Size(489, 0);
+            this.cogToolBlockEditV21.Name = "cogToolBlockEditV21";
+            this.cogToolBlockEditV21.ShowNodeToolTips = true;
+            this.cogToolBlockEditV21.Size = new System.Drawing.Size(808, 410);
+            this.cogToolBlockEditV21.SuspendElectricRuns = false;
+            this.cogToolBlockEditV21.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.cogToolBlockEditV21);
+            this.Controls.Add(this.axCogDisplay1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axCogDisplay1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cogToolBlockEditV21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxCognex.VisionPro.Interop.AxCogDisplay axCogDisplay1;
+        private Cognex.VisionPro.ToolBlock.CogToolBlockEditV2 cogToolBlockEditV21;
+    }
+}
+
